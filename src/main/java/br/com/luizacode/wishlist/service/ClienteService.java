67d6760f3cd5d539227d89e04f8608eb8bf7964e
andreagnecco <andreagnecco@gmail.com>
2021-05-13 17:45:58 -0300

    Cria Cliente repository e service

---
+++ src/main/java/br/com/luizacode/wishlist/service/ClienteService.java
@@ -0,0 +1,26 @@
+package br.com.luizacode.wishlist.service;
+
+import br.com.luizacode.wishlist.entity.Cliente;
+import br.com.luizacode.wishlist.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public Cliente adicionarCliente(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    public Cliente buscarCliente(Long id) {
+        return clienteRepository.findById(id.longValue());
+    }
+
+    public Cliente atualizarCliente(Cliente cliente){
+        return clienteRepository.save(cliente);
+    }
+
+}
