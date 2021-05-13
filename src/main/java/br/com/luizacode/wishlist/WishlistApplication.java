3ecfa7eb682bef8c8b7584733ea163d12599df87
andreagnecco <andreagnecco@gmail.com>
2021-05-13 17:21:21 -0300

    Remove CommandLineRunner

--- src/main/java/br/com/luizacode/wishlist/WishlistApplication.java
+++ src/main/java/br/com/luizacode/wishlist/WishlistApplication.java
@@ -1,22 +1,13 @@
 package br.com.luizacode.wishlist;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WishlistApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WishlistApplication.class, args);
-	}
-
-	@Bean
-	public CommandLineRunner commandLineRunner() {
-		return args -> {
-			System.out.println("---------------- WE ROCK DREAM WOMEN!!! ---------------");
-		};
 	}
 
 }
