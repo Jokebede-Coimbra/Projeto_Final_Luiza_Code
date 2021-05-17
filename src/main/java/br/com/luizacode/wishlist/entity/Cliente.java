8ad405d78e72b470967c0df3544ed019db00e4d6
Rosi556 <rosicleite-silva@hotmail.com>
2021-05-17 17:45:03 -0300

    Adiciona controller do Cliente

--- src/main/java/br/com/luizacode/wishlist/entity/Cliente.java
+++ src/main/java/br/com/luizacode/wishlist/entity/Cliente.java
@@ -41,4 +41,35 @@ public class Cliente implements Serializable {
         this.senha = senha;
     }
 
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
